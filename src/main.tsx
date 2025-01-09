55cd5c73e5866b6b6d158b5c9811c5d0abffcf8c
Juan Olvera <juan@jolvera.dev>
2025-01-09 12:06:47 -0600

    feat: add eui

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { EuiProvider } from "@elastic/eui";
 import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <EuiProvider>
+      <App />
+    </EuiProvider>
   </StrictMode>,
 );
