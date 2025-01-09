8a8d58d074440ba6a6a6ad24acd0c6180e8d0ace
Juan Olvera <juan@jolvera.dev>
2025-01-09 12:40:15 -0600

    chore: add api proxy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,15 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
   plugins: [TanStackRouterVite(), react()],
 });
