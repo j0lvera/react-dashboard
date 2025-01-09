7c35a5c5e5baa8497a1b72f4819d1de5e4861331
Juan Olvera <juan@jolvera.dev>
2025-01-09 12:16:06 -0600

    feat: tanstack router basic setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { EuiProvider } from "@elastic/eui";
-import "./index.css";
-import App from "./App.tsx";
+
+import { routeTree } from "./routeTree.gen.ts";
+
+const router = createRouter({
+  routeTree,
+});
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <EuiProvider>
-      <App />
+      <RouterProvider router={router} />
     </EuiProvider>
   </StrictMode>,
 );
