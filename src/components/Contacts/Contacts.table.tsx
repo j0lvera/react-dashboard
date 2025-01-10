3c72aec5c54c53981c9aa8250b162e420e9eba61
Juan Olvera <juan@jolvera.dev>
2025-01-10 09:18:49 -0600

    feat(table): add pagination

--- src/components/Contacts/Contacts.table.tsx
+++ src/components/Contacts/Contacts.table.tsx
@@ -1,8 +1,19 @@
-import { EuiBasicTable, EuiBasicTableColumn } from "@elastic/eui";
+import { useState } from "react";
+import {
+  Criteria,
+  EuiTableFieldDataColumnType,
+  EuiBasicTable,
+  EuiBasicTableColumn,
+  EuiText,
+  EuiSpacer,
+  EuiHorizontalRule,
+} from "@elastic/eui";
 import type { Contact, ContactsTableComponent } from "./Contacts.type.ts";
-import { EuiTableFieldDataColumnType } from "@elastic/eui/src/components/basic_table/table_types";
 
 const ContactsTable: ContactsTableComponent = ({ data }) => {
+  const [pageIndex, setPageIndex] = useState(0);
+  const [pageSize, setPageSize] = useState(0);
+
   const columns: EuiBasicTableColumn<Contact>[] = [
     {
       field: "name",
@@ -41,15 +52,83 @@ const ContactsTable: ContactsTableComponent = ({ data }) => {
     textOnly: true,
   });
 
+  // Pagination logic
+  const onTableChange = ({ page }: Criteria<Contact>) => {
+    if (page) {
+      const { index, size } = page;
+      setPageIndex(index);
+      setPageSize(size);
+    }
+  };
+
+  // Manually handle pagination
+  const findContacts = (
+    contacts: Contact[],
+    pageIndex: number,
+    pageSize: number,
+  ) => {
+    let pageOfItems;
+    const len = contacts.length;
+
+    if (!pageIndex && !pageSize) {
+      pageOfItems = contacts;
+    } else {
+      const startIndex = pageIndex * pageSize;
+      pageOfItems = contacts.slice(
+        startIndex,
+        Math.min(startIndex + pageSize, len),
+      );
+    }
+
+    return {
+      pageOfItems,
+      totalItemCount: len,
+    };
+  };
+
+  const { pageOfItems, totalItemCount } = findContacts(
+    data,
+    pageIndex,
+    pageSize,
+  );
+
+  const pagination = {
+    pageIndex,
+    pageSize,
+    totalItemCount,
+    pageSizeOptions: [10, 0],
+  };
+
+  const resultsCount =
+    pageSize === 0 ? (
+      <strong>All</strong>
+    ) : (
+      <>
+        <strong>
+          {pageSize * pageIndex + 1}-{pageSize * pageIndex + pageSize}
+        </strong>{" "}
+        of {totalItemCount}
+      </>
+    );
+
   return (
-    <EuiBasicTable
-      tableCaption={"Contacts table"}
-      items={data}
-      rowHeader={"name"}
-      rowProps={getRowProps}
-      cellProps={getCellProps}
-      columns={columns}
-    />
+    <>
+      <EuiText size="xs">
+        Showing {resultsCount} <strong>Users</strong>
+      </EuiText>
+      <EuiSpacer size={"s"} />
+      <EuiHorizontalRule margin={"none"} style={{ height: 2 }} />
+      <EuiBasicTable
+        tableCaption={"Contacts table"}
+        items={pageOfItems}
+        rowHeader={"name"}
+        rowProps={getRowProps}
+        cellProps={getCellProps}
+        columns={columns}
+        onChange={onTableChange}
+        pagination={pagination}
+      />
+    </>
   );
 };
 
